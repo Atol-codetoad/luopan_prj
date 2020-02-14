0cf092b8e40b135a81b40555c9b72f3e66bf950d
Atol-codetoad <atolasov@gmail.com>
2020-02-14 23:59:59 +0300

    commit 25 delphi

--- Window_test.pas
+++ Window_test.pas
@@ -22,7 +22,7 @@ type
     { Private declarations }
   public
     { Public declarations }
-    procedure DrawBaGua(Canva: TCanvas);
+    procedure DrawBaGua1(Canva: TCanvas);
     procedure createYuing(Canva: TCanvas; start, stop: TPointF);
   end;
 
@@ -36,15 +36,20 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   num: integer;
+  Bagua:TBagua;
 begin
-  num := gua_func.CalcGuaNum(self.DateEdit1.DateTime,
+  Bagua:=TBagua.Create;
+  num := Bagua.CalcGuaNum(self.DateEdit1.DateTime,
     self.RadioButton1.IsChecked);
   self.Label1.Text := inttostr(num);
 
   self.Canvas.Lock;
-  DrawBaGua(self.Canvas);
+  //DrawBaGua1(self.Canvas);
+  Bagua.DrawBaGua(self.Canvas,num );
   self.Canvas.Unlock;
   // self.Invalidate; //repaint
+  Bagua.DisposeOf;
+  Bagua:=nil;
 end;
 
 procedure TForm1.createYuing(Canva: TCanvas; start, stop: TPointF);
@@ -56,7 +61,7 @@ procedure TForm1.createYuing(Canva: TCanvas; start, stop: TPointF);
 
 var
   DefColor: TAlphaColor;
-  qtr1, qtr3, half,qtr12, qtr32: TPointF;
+  qtr1, qtr3, half, qtr12, qtr32: TPointF;
 begin
   DefColor := Canva.Stroke.Color;
   Canva.Stroke.Color := TAlphaColors.Beige;
@@ -64,29 +69,26 @@ begin
   // --------------------------------------------
   half := HalfLine(start, stop);
   qtr1 := HalfLine(start, half);
-  qtr12 :=HalfLine(qtr1, half);
+  qtr12 := HalfLine(qtr1, half);
   qtr3 := HalfLine(half, stop);
-  qtr32:=HalfLine( half,qtr3);
-  //------------------------------
+  qtr32 := HalfLine(half, qtr3);
+  // ------------------------------
   Canva.DrawLine(qtr12, qtr32, 100);
   // -------------------------------------------
   Canva.Stroke.Color := DefColor;
 end;
 
-procedure TForm1.DrawBaGua(Canva: TCanvas);
+procedure TForm1.DrawBaGua1(Canva: TCanvas);
 var
   Centr_Point: Tpoint;
   paint_cord: array [0 .. 2] of TPolygon;
   i, j, radius: integer;
   radian: real;
-  DefColor: TAlphaColor; //?
-  const
-  BaGuaYuing:array [0..2,0..7]of boolean =
-  (
-  (true,false,true,false,false,true,true,false),
-  (false,true,true,false,true,true,false,false ),
-  (true,true,false,true,false,true,false,false)
-  );
+  DefColor: TAlphaColor; // ?
+const
+  BaGuaYuing: array [0 .. 2, 0 .. 7] of boolean = ((true, false, true, false,
+    false, true, true, false), (false, true, true, false, true, true, false,
+    false), (true, true, false, true, false, true, false, false));
 
 begin
   Centr_Point.X := (Canva.Width - 100) div 2;
@@ -98,17 +100,17 @@ begin
 
   for i := 0 to 2 do
   begin
-    SetLength(paint_cord[i], 9);
-    radian := 49*Pi/36;     //245 градусов  отнимать от 270(3*pi/2)-25
-    //radian := 3*pi/2; //radian :=grad*pi/180
+    SetLength(paint_cord[i], 8);
+    radian := 49 * Pi / 36; // 245 градусов  отнимать от 270(3*pi/2)-25
+    // radian := 3*pi/2; //radian :=grad*pi/180
     for j := 0 to 7 do
     begin
       paint_cord[i][j].X := Centr_Point.X + round(radius * cos(radian));
       paint_cord[i][j].Y := Centr_Point.X - round(radius * sin(radian));
-      radian := radian + pi / 4;
+      radian := radian + Pi / 4;
     end;
 
-    paint_cord[i][8] := paint_cord[i][0];
+   // paint_cord[i][8] := paint_cord[i][0];
     // ----------------------------------
 
     Canva.BeginScene();
@@ -116,18 +118,18 @@ begin
     Canva.DrawPolygon(paint_cord[i], 100);
     for j := 0 to 7 do
     begin
-     //-------------------------------------
-     DefColor := Canva.Stroke.Color;
-     Canva.Stroke.Color := TAlphaColors.Beige;
-     Canva.Stroke.Thickness := 6;
-     Canva.DrawLine( paint_cord[i][j], paint_cord[i][j],100);
-     Canva.Stroke.Color := DefColor;
-     Canva.Stroke.Thickness := 5;
-     //-------------------------------------
+      // -------------------------------------
+      DefColor := Canva.Stroke.Color;
+      Canva.Stroke.Color := TAlphaColors.Beige;
+      Canva.Stroke.Thickness := 6;
+      Canva.DrawLine(paint_cord[i][j], paint_cord[i][j], 100);
+      Canva.Stroke.Color := DefColor;
+      Canva.Stroke.Thickness := 5;
+      // -------------------------------------
       if BaGuaYuing[i][j] then
-        createYuing(Canva, paint_cord[i][j], paint_cord[i][j+1]);
+        createYuing(Canva, paint_cord[i][j], paint_cord[i][j + 1]);
     end;
-    Canva.EndScene();
+   // Canva.EndScene();
     radius := radius - 15;
   end;
 end;
--- gua_func.pas
+++ gua_func.pas
@@ -4,14 +4,58 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
-  System.Variants,
+  System.Variants, FMX.Graphics,
   FMX.Types;
 
-function CalcGuaNum(DateBirth: TDateTime; sex: boolean): Integer; // sex True=M
+type
+ TCompasPoint =(N,NE,E,SE,S,SW,W,NW);
 
+  TGuaProp = class(TObject)
+
+    HaoSe, BuhaoSe: TAlphaColor;
+
+  public
+    Coord: Tpoint;
+    isYin: Boolean; // def false ;
+    isHao: Boolean;
+    constructor Create;
+  end;
+
+  TBagua = class(TPersistent)
+  private
+   Zi_Radus:Integer ;
+   BaguaFen: array [0 .. 2, 0 .. 7] of TGuaProp;
+   HanZi:Array [0 .. 7] of TGuaProp;//времменно
+   procedure createYuing(Canva: TCanvas; start, stop: TPointF);
+  public
+   constructor Create;
+   function CalcGuaNum(DateBirth: TDateTime; sex: Boolean): Integer;
+    // sex True=M
+    Procedure DrawBaGua(Canva: TCanvas;GuaNum:Integer);
+    Procedure DrawHanZi(Canva:TCanvas;GuaNum:Integer);
+  end;
+
+Const
+  DEF_GOOD = TAlphaColorRec.Blueviolet;
+  DEF_NOGOOD = TAlphaColorRec.Deeppink;
+  BAGUA_YIN: array [0 .. 2, 0 .. 7] of boolean = (
+  (true, false, true, false, false, true, true, false),
+  (false, true, true, false, true, true, false, false),
+  (true, true, false, true, false, true, false, false)
+  );
+  MAP_GOODSTATE_GUANUM: array [0..7,0..7] of boolean =(  //1 -PosNumGua,2GuaHao
+   (true,false,true,true,true,false,false,false),
+   (false,true,false,false,false,true,true,true),
+   (true,false,true,true,true,false,false,false),
+   (true,false,true,true,true,false,false,false),
+   (false,true,false,false,false,true,true,true),
+   (false,true,false,false,false,true,true,true),
+   (false,true,false,false,false,true,true,true),
+   (true,false,true,true,true,false,false,false)
+   );
 implementation
 
-function CalcGuaNum(DateBirth: TDateTime; sex: boolean): Integer;
+function TBagua.CalcGuaNum(DateBirth: TDateTime; sex: Boolean): Integer;
 var
   myYear, myMonth, myDay: word;
   // ------------- inner func
@@ -40,7 +84,7 @@ var
     result := Twoin1(l2d); // ?
   end;
 
-  function res5(num: Integer; sex: boolean): Integer;
+  function res5(num: Integer; sex: Boolean): Integer;
   begin
     if num = 5 then
     begin
@@ -85,5 +129,161 @@ begin
   end;
 
 end;
+
+constructor TBagua.Create;
+var i,j:integer;
+begin
+  for I := 0 to 2 do
+    for J := 0 to 7 do
+     begin
+       self.BaguaFen[i][j]:=TGuaProp.Create;
+     end;
+end;
+
+procedure TBagua.DrawBaGua(Canva: TCanvas; GuaNum: Integer);
+ var
+  Centr_Point,slice,slice2: Tpoint;
+  tmp_Point,tmp_Point2 : Tpoint;
+  i, j,k,m, radius: integer;
+  radian: real;
+  DefColor: TAlphaColor; // ?
+  DefThink:single;
+const
+  BaGuaYuing: array [0 .. 2, 0 .. 7] of boolean = ((true, false, true, false,
+    false, true, true, false), (false, true, true, false, true, true, false,
+    false), (true, true, false, true, false, true, false, false));
+
+begin
+ DefColor := Canva.Stroke.Color;
+ DefThink:=Canva.Stroke.Thickness;
+
+  //  Get Center
+  Centr_Point.X := (Canva.Width - 100) div 2;
+  Centr_Point.Y := (Canva.Height - 200) div 2;
+  radius := (Canva.Height - 100) div 3;
+
+  self.Zi_Radus:= radius + 20;
+  // ----Set Coords Bagua Feng
+
+  for i := 0 to 2 do
+  begin
+
+    radian := 49 * Pi / 36; // 245 градусов  отнимать от 270(3*pi/2)-25
+    // radian := 3*pi/2; //radian :=grad*pi/180
+    for j := 0 to 7 do
+    begin
+      self.BaguaFen[i][j].Coord.X := Centr_Point.X + round(radius * cos(radian));
+      self.BaguaFen[i][j].Coord.Y := Centr_Point.X - round(radius * sin(radian));
+      radian := radian + Pi / 4;
+    end;
+    //----------------Draw bagua----------------------------------
+    for j := 0 to 7 do
+    begin
+     Canva.BeginScene();
+     Canva.Stroke.Thickness := 6;
+       if MAP_GOODSTATE_GUANUM [GuaNum][j] then
+         Canva.Stroke.Color:= DEF_GOOD
+       else
+         Canva.Stroke.Color:= DEF_NOGOOD;
+      //-----------------------------------------------------
+        tmp_Point.X:=  self.BaguaFen[i][j].Coord.X;
+        tmp_Point.Y:=  self.BaguaFen[i][j].Coord.Y;
+       if j<7 then
+        begin
+        tmp_Point2.X:=  self.BaguaFen[i][j+1].Coord.X;
+        tmp_Point2.Y:=  self.BaguaFen[i][j+1].Coord.Y;
+        end
+       else
+        begin
+        tmp_Point2.X:=  self.BaguaFen[i][0].Coord.X;
+        tmp_Point2.Y:=  self.BaguaFen[i][0].Coord.Y;
+        end;
+      //============================================
+         slice :=tmp_Point2 ;
+         slice2 := tmp_Point;
+         //--------------------14.02
+         m:=2;
+         if i>1 then   m:=3;
+         //--------------------14.02
+         for k := 0 to m do
+          begin
+           slice.X := round((tmp_Point.X+slice.X) /2) ;
+           slice.Y := round((tmp_Point.Y+slice.Y) /2) ;
+           slice2.x := round((slice2.x + tmp_Point2.x) /2) ;
+           slice2.y := round((slice2.y + tmp_Point2.y) /2) ;
+          end;
+           tmp_Point:=slice;
+           tmp_Point2:=slice2;
+      //-----------------------------------------------------
+       if BAGUA_YIN[i][j] then
+        createYuing(Canva, tmp_Point, tmp_Point2)
+       else
+        Canva.DrawLine( tmp_Point, tmp_Point2, 100);
+
+     Canva.EndScene;
+    end;
+     radius := radius - 20;
+  end;
+   //----------------------------------------------------------
+
+
+  Canva.Stroke.Color := DefColor;
+  Canva.Stroke.Thickness:= DefThink;
+end;
+
+
+
+
+procedure TBagua.createYuing(Canva: TCanvas; start, stop: TPointF);
+  function HalfLine(p1, p2: TPointF): TPointF;
+  begin
+    result.X := (p1.X + p2.X) / 2;
+    result.Y := (p1.Y + p2.Y) / 2;
+  end;
+
+var
+  DefColor: TAlphaColor;
+  qtr1, qtr3, half, qtr12, qtr32: TPointF;
+begin
+  // --------------------------------------------
+  half := HalfLine(start, stop);
+  qtr1 := HalfLine(start, half);
+  qtr12 := HalfLine(qtr1, half);
+  qtr3 := HalfLine(half, stop);
+  qtr32 := HalfLine(half, qtr3);
+  // ------------------------------
+  Canva.DrawLine(start,qtr12, 100);
+  Canva.DrawLine(qtr32,stop, 100);
+  // -------------------------------------------
+  Canva.Stroke.Color := DefColor;
+end;
+
+ procedure TBagua.DrawHanZi(Canva: TCanvas; GuaNum: Integer);
+begin
+{    radian := 49 * Pi / 36; // 245 градусов  отнимать от 270(3*pi/2)-25
+    // radian := 3*pi/2; //radian :=grad*pi/180
+    for j := 0 to 7 do
+    begin
+      self.HanZi[i][j].Coord.X := Centr_Point.X + round(radius * cos(radian));
+      self.HanZi[i][j].Coord.Y := Centr_Point.X - round(radius * sin(radian));
+      radian := radian + Pi / 4;
+    end;
+
+    Canva.TextToPath()  }
+end;
+
+
+{ TGuaProp }
+
+constructor TGuaProp.Create;
+begin
+  self.Coord.X := 0;
+  self.Coord.Y := 0;
+  self.isYin := false;
+  self.HaoSe := DEF_GOOD;
+  self.BuhaoSe := DEF_NOGOOD;
+end;
+
+
 
 end.
